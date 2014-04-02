57e178b2ea5c78570aec75e22b6b24ef032c5dfd
Alex M <bogdanmarginean@ymail.com>
2014-04-02 19:22:57 +0300

    Added stubs for characters & their stats.

---
+++ Assets/Scripts/Shared/Party/Character.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterStats))]
+public class Character : MonoBehaviour 
+{
+	private CharacterStats _stats;
+
+	public CharacterStats Stats
+	{
+		get
+		{
+			return _stats;
+		}
+	}
+
+	public void Start()
+	{
+		_stats = GetComponent<CharacterStats>();
+		if (_stats == null)
+		{
+			Debug.LogError("No stats attached to character! " + gameObject.name);
+		}
+	}
+}
---
+++ Assets/Scripts/Shared/Party/CharacterStats.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStats : MonoBehaviour 
+{
+	public int Health;
+	public int Mana;
+	public int Strength;
+	public int Wisdom;
+}
